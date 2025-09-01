551759a8f4f8e2f58ea55e5b50af16b0cf42d665
shreesh <sgupta182005@gmail.com>
2025-09-01 15:53:00 +0530

    Add files via upload

---
+++ q1_A4.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+#define MAX 5
+
+int queue[MAX];
+int front = -1;
+int rear = -1;
+
+bool isFull()
+{
+    return (rear == MAX - 1);
+}
+
+bool isEmpty()
+{
+    return (front == -1 || front > rear);
+}
+
+void enqueue(int x)
+{
+    if(isFull())
+    {
+        cout << "Queue is full \n"<<endl;
+        return;
+    }
+    
+    if(front == -1)
+    {
+        front = 0;
+        rear = 0;
+    }
+    
+    else 
+    {
+        rear = rear + 1;
+    }
+    queue[rear] = x;
+}
+
+int dequeue() 
+{
+    if(isEmpty())
+    {
+        cout << "Queue is empty" <<endl;
+        return -1;
+    }
+    else
+    {
+        int x = queue[front];
+        front = front + 1;
+        if (front > rear)
+        {
+            front = -1;
+            rear = -1;
+        }
+        return x;
+    }
+}
+
+int display()
+{
+    if(isEmpty())
+    {
+        cout << "Queue is empty \n" << endl;
+        return -1;
+    }
+    
+    for(int i = front ; i <= rear ; i++)
+    {
+        cout << queue[i] << " " ;
+    }
+    return 0;
+}
+
+int peek()
+{
+    if(isEmpty())
+    {
+        cout << "Queue is empty \n" << endl;
+        return -1;
+    }
+    
+    else
+    {
+        return queue[front];
+    }
+}
+
+int main()
+{
+    enqueue(10);
+    cout << "front element of queue: "<< peek() << endl;
+    enqueue(20);
+    enqueue(30);
+    display();
+    cout << endl;
+    cout << "Dequeued: " << dequeue() <<endl;
+    cout << "front element of queue: "<< peek() << endl;
+    cout << "Dequeued: " << dequeue() <<endl;
+    cout << "front element of queue: "<< peek() << endl;
+    cout << "Dequeued: " << dequeue() <<endl;
+    cout << "front element of queue: "<< peek() << endl;
+    return 0;
+}
