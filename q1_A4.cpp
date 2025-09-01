f558b130dc7ed4f27e61faf46b5bc444b7a93d46
shreesh <sgupta182005@gmail.com>
2025-09-01 16:30:50 +0530

    Update q1_A4.cpp

--- q1_A4.cpp
+++ q1_A4.cpp
@@ -102,3 +102,100 @@ int main()
     cout << "front element of queue: "<< peek() << endl;
     return 0;
 }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~METHOD - 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#include<iostream>
+using namespace std;
+#define SIZE 3
+
+class Queue
+{
+    int arr[SIZE];
+    int front;
+    int rear;
+
+    public:
+
+    Queue()
+    {
+        front = 0;
+        rear = 0;
+    }
+
+    void enqueue(int val)
+    {
+        if(rear == SIZE)
+            cout<<"Queue is Full"<<endl;
+        else
+        {
+            arr[rear] = val;
+            rear++;
+        }
+    }
+    
+    
+    void dequeue()
+    {
+        if(front == rear)
+            cout<<"Queue is Empty"<<endl;
+        else
+        {
+            cout<<"Dequeued element= "<<arr[front]<<endl;
+            front++;
+        }
+    }
+    
+    void display()
+    {
+        if(front == rear)
+        {
+            cout<<"Queue is empty"<<endl;
+        }
+        else
+        {
+            for(int i = front; i < rear; i++)
+            {
+                cout << arr[i] <<" ";
+            }
+        }
+    }
+    
+    void peek()
+    {
+        if(front == rear)
+        {
+            cout<<"Queue is empty"<<endl;
+        }
+        else
+        {
+            cout<<"front element: "<<arr[front]<<endl;
+        }
+    }
+};
+
+int main()
+{
+    Queue q;
+
+    cout<<"Enqueuing element= "<<10<<endl;
+    q.enqueue(10);
+    cout<<"Enqueuing element= "<<20<<endl;
+    q.enqueue(20);
+    cout<<"Enqueuing element= "<<30<<endl;
+    q.enqueue(30);
+    q.peek();
+    
+    q.display();
+    
+    cout << endl;
+    
+    q.dequeue();
+    q.peek();
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+
+    return 0;
+}
+
